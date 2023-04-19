22fe1ab206fa96b839cf07f53746932d94778f30
TihorSam <108570778+TihorSam@users.noreply.github.com>
2023-04-19 12:58:09 +0530

    Update sensorDatabase.py

--- Assignments/sensorDatabase.py
+++ Assignments/sensorDatabase.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as paho
-from myDatabase import storeData
+
 #pip3 install paho-mqtt
 global mqttclient;
 global broker;
@@ -12,6 +12,23 @@ port = 1883;
 client_uniq = "pubclient_123"
 
 mqttclient = paho.Client(client_uniq, True) 
+def storeData(mesg):
+  #Create a connection to MySQL Database 
+  conn =pymysql.connect(database="senorData",user="user",password="pass123",host="localhost")
+
+  #Create a MySQL Cursor to that executes the SQLs
+  cur=conn.cursor()
+  #Create a dictonary containing the fields, name, age and place
+  data={'topic':'IOT/test','data':mesg}
+  #Execute the SQL to write data to the database
+  cur.execute("INSERT INTO MQTTdata(topic , data)VALUES(%(topic)s,%(data)s);",data)
+  print("Data added")
+  #Close the cursor
+  cur.close()
+  #Commit the data to the database
+  conn.commit()
+  #Close the connection to the database
+  conn.close()
 
 def test(client, userdata, message):
   print("client:"+ str(client))
