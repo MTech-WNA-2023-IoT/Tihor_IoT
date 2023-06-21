a5cef5a73944117cacc4c80d8c4bb244b7483000
TihorSam <108570778+TihorSam@users.noreply.github.com>
2023-06-21 22:37:20 +0530

    Create mqtt_pub_data.py

---
+++ Project/mqtt_pub_data.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+
+# MQTT broker configuration
+mqttBroker = "35.200.145.127"
+mqttPort = 1883
+mqttTopic = "sensor/accelerometer"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker")
+    client.subscribe(mqttTopic)
+
+def on_message(client, userdata, msg):
+    print("Received message: " + str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+print("Connecting to MQTT broker...")
+client.connect(mqttBroker, mqttPort, 60)
+
+client.loop_forever()
